2573d1fdef771c6e63bf7d84ad9c9bf330d7321f
iniasdb <inias.debelder@hotmail.be>
2023-01-22 15:21:18 +0100

    test

---
+++ GithubHelper.py
@@ -0,0 +1,42 @@
+from git import Repo
+import os
+import subprocess
+import datetime
+
+class GithubHelper:
+    def __init__(self) -> None:
+        self.repo = Repo.init("./root")
+    
+    def add_files(self):
+        index = self.repo.index
+        index.add(".")
+        index.commit(str(datetime.datetime.now()))
+    
+# h = GithubHelper()
+# h.add_files()
+
+if __name__ == "__main__":
+    # repo_path = os.getenv('REPO_PATH')
+    # Repo object used to interact with Git repositories
+    # try:
+    #     repo = Repo("./root")
+    # except:
+    #     repo = Repo.init("./root")
+
+    # # check that the repository loaded correctly
+    # if not repo.bare:
+    #     print('Repo at ./root successfully loaded.')
+    #     index = repo.index
+    #     index.add("markdown")
+    #     index.add("pdf")
+    #     index.commit(message="test")
+    #     print(repo.git.status())
+
+    # else:
+    #     print('Could not load repository at ./root :')
+    os.system("cd root")
+    os.system("git init") # No harm in initializing multiple times
+    os.system("git add .")
+    os.system("git commit -m test")
+    # message = os.popen("git ls-remote --exit-code").read()
+    # print(message)
